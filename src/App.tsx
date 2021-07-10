2050c7f161b2ca4d77a2ebd87f4002bd6a8339ee
thomas-fm <13519099@std.stei.itb.ac.id>
2021-07-10 13:22:09 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import Main from "./components/Main"
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Main/>
+    </div>
+  )
+}
+
+export default App
